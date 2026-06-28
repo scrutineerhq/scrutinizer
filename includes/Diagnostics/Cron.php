d1348c5891350aafdd591308d8236f3352a1e5ef
Kane <kane@hatch.ai>
2026-06-28 15:55:54 -0700

    Fix PHPCS alignment warnings

--- includes/Diagnostics/Cron.php
+++ includes/Diagnostics/Cron.php
@@ -33,9 +33,9 @@ class Cron {
 	public static function collect() {
 		$cron_array = _get_cron_array();
 		$schedules  = wp_get_schedules();
-		$now      = time();
-		$events   = array();
-		$warnings = array();
+		$now        = time();
+		$events     = array();
+		$warnings   = array();
 
 		if ( ! is_array( $cron_array ) ) {
 			return array(
@@ -129,9 +129,8 @@ class Cron {
 					break;
 				}
 			}
-			$threshold = $interval > 0 ? $interval * 2 : 600;
-
-			$close_count    = 0;
+			$threshold       = $interval > 0 ? $interval * 2 : 600;
+			$close_count     = 0;
 			$timestamp_count = count( $timestamps );
 			for ( $ci = 1; $ci < $timestamp_count; $ci++ ) {
 				if ( ( $timestamps[ $ci ] - $timestamps[ $ci - 1 ] ) < $threshold ) {
